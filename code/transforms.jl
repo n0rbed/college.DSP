a11cb038e77722ab73def7db9f478873eb016403
n0rbed <yassinahmad789@gmail.com>
2025-03-08 16:31:11 +0200

    increase performance by computing twiddle only once

--- code/transforms.jl
+++ code/transforms.jl
@@ -1,4 +1,24 @@
+####### TWIDDLE #######
 W(m, N) = exp(-im*2pi*m/N)
+
+function comp_twiddle(r, N)
+    factors = []
+    for m = 0:r-1
+        push!(factors, W(m, N))
+    end
+    return factors
+end
+
+# for all powers of 2 in range N, comp twiddle needed only once
+function get_twiddle(N)
+    twiddle = []
+    power_N = log2(N)
+    for i = 1:power_N
+        push!(twiddle, comp_twiddle((2^i)/2, 2^i))
+    end
+    twiddle
+end
+
 
 function dft(x)
     N = length(x)
@@ -11,17 +31,15 @@ function dft(x)
     X
 end
 
-function get_twiddle(r, N)
-    factors = []
-    for m = 0:r-1
-        push!(factors, W(m, N))
-    end
-    return factors
-end
-
+########## RADIX 2 FFT #############
 function radix2(x)
     N = length(x)
     @assert isinteger(log2(N))
+    twiddle = get_twiddle(N)
+    radix2_recurse(x, N, twiddle)
+end
+
+function radix2_recurse(x, N, twiddle)
     if N == 2
         return [(x[1] + x[2]); (x[1] - x[2])]
     end
@@ -36,9 +54,14 @@ function radix2(x)
         end
     end
 
-    twiddle = get_twiddle(N/2, N)
+    A = radix2_recurse(x_even, N/2, twiddle)
+    B = radix2_recurse(x_odd, N/2, twiddle) 
 
-    X = [radix2(x_even) .+ twiddle .* radix2(x_odd) 
-        radix2(x_even) .- twiddle .* radix2(x_odd)]
+    X = [A .+ twiddle[Int(log2(N))] .* B
+        A .- twiddle[Int(log2(N))] .* B]
     return X
+end
+
+
+function dft2d(x)
 end
